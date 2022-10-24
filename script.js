e3b7e452b54c790f5bfed640e64897a2b9800073
taianaazevedo <taianazev@gmail.com>
2022-10-24 15:16:00 -0300

    jogo com o alerta de fim de jogo

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//pergunta a quantidade de cartas:
- 
+//pergunta a quantidade de cartas: 
 let qntd = prompt('Com quantas cartas você quer jogar?')
 
 while(qntd < 4 || qntd > 14 || (qntd % 2) !== 0){
@@ -8,7 +7,6 @@ while(qntd < 4 || qntd > 14 || (qntd % 2) !== 0){
 
 
 //cria a array com os gifs:
-
 const periquitos = [
     'bobrossparrot', 
     'explodyparrot', 
@@ -18,42 +16,37 @@ const periquitos = [
     'tripletsparrot', 
     'unicornparrot'
 ]
-
 const gifs = [];
 
+// na função, a quantidade deve ser dividida por 2, para que os gifs tenham seus pares
 function criaGifs(){
-// a quantidade deve ser dividida por 2, para que os gifs tenham seus pares
+
 for(i = 0; i < (qntd / 2); i++){
         const versoGif = periquitos[i];
         gifs.push(versoGif) + gifs.push(versoGif);
-
-        
+     
 }
-
 }
 criaGifs();
 
 //embaralhar as cartas
-
 gifs.sort(comparador)
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-//carta completa: com a frente e o verso.
 
-
+//card: cartinha com a frente e o verso.
 let card;
 
 //criar cartas - aula de quarta-feira
-
 function criarCartas(){
 
     const listaCartas = document.querySelector('div .conteudo-cartas');
     
     for(i = 0; i < gifs.length; i++){    
            
-        let card = `<div class="carta" data-periquito="${gifs[i]}"onclick="flipCarta(this)">        
+        let card = `<div class="carta" data-periquito="${gifs[i]}" onclick="flipCarta(this)">        
         <div class="lado frente"></div>
         <div class="lado verso"><img src="./imagens/${gifs[i]}.gif"</div>
         </div>`;
@@ -64,20 +57,26 @@ function criarCartas(){
 }
 criarCartas();
 
+//variáveis globais que vão guardar as informações das cartas clicadas:
 let primeiraCarta = '';
 let segundaCarta = '';
+let parFormado = 0;
 
+
+//função que verifica se as cartas são iguais
 function checaCartas(){
+    
     const primeiroPeriquito = primeiraCarta.getAttribute('data-periquito');
-    const SegundoPeriquito = segundaCarta.getAttribute('data-periquito');
+    const segundoPeriquito = segundaCarta.getAttribute('data-periquito');    
 
-    if(primeiroPeriquito === SegundoPeriquito){
+    if(primeiroPeriquito === segundoPeriquito){
         
         primeiraCarta = '';
         segundaCarta = '';
-            
 
-
+        parFormado++
+        fimJogo();
+  
     } else {
 
         setTimeout(() => {
@@ -93,9 +92,11 @@ function checaCartas(){
 }
 checaCartas();
 
+//função que vira as cartas
 function flipCarta(cartaSelecionada){
-    
-  if (cartaSelecionada.classList.contains('virada')){
+        
+  if (cartaSelecionada.classList.contains('virada')){ 
+      
     return;
   }
   if( primeiraCarta === ''){
@@ -105,7 +106,20 @@ function flipCarta(cartaSelecionada){
     cartaSelecionada.classList.add('virada');
     segundaCarta = cartaSelecionada;
 
-  }
+  }  
+  
   checaCartas();
+
+
 }
 flipCarta();
+
+function fimJogo(){
+  if(parFormado === (qntd / 2)){
+    setTimeout(() => {
+      alert('Parabéns! Você venceu o jogo! =D')
+    }, 2000);
+    
+  }
+}
+
